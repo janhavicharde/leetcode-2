9ea0414cbcbfefdaea1974f41d8933e294a5cd7e
CHARDE JANHAVI RAJU <janhavicharde@gmail.com>
2026-01-17 01:20:49 +0530

    remove paranthesis

---
+++ binary search/remove paranthesis.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();  
+        
+        int count = 0;  
+
+       
+        for (char ch : s.toCharArray()) {
+            
+            if (ch == '(') {
+                
+                if (count> 0) result.append(ch);
+                 
+                count++; 
+            } 
+            
+            else if (ch == ')') {
+                
+                count--;  
+                
+                if (count> 0) result.append(ch);
+            }
+        }
+
+        
+        return result.toString();
+
+        
+    }
+}
